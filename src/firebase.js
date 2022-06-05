50796f2d7b7de1bfadd1e462a6cae8703f26451d
Natalia2806 <91932527+NataliaTuiran2806@users.noreply.github.com>
2022-06-04 21:16:35 -0500

    Añadiendo archivo firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+import 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC-3fntNJMOTDrtrqnfpimQc5xW7g0c4nc",
+    authDomain: "rick-morty-84465.firebaseapp.com",
+    projectId: "rick-morty-84465",
+    storageBucket: "rick-morty-84465.appspot.com",
+    messagingSenderId: "1006753627567",
+    appId: "1:1006753627567:web:86f29fbc424a26bdf6035a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export{firebase}
