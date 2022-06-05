3521e9bd2457f6933dd009a43dc863e2497f859e
Natalia2806 <91932527+NataliaTuiran2806@users.noreply.github.com>
2022-06-04 20:44:35 -0500

    Creacion del componente Card y listado de personajes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,52 @@
 import Logo from './assets/logo.png'
 import { useState, useEffect } from 'react'
+import { Search } from './components/Search'
+import { Card } from './components/Card'
+
+
 function App() {
   const [personajes, setPersonajes] = useState([])
-	const [loading, setLoading] = useState(true)
-	const [filter, setFilter] = useState('')
+  const [loading, setLoading] = useState(true)
+  const [filter, setFilter] = useState('')
 
   //Realizamos llamado a la API
   useEffect(() => {
-		const getPersonajes = async () => {
-			try {
-				const response = await fetch(
-					'https://rickandmortyapi.com/api/character/?name=Baby&status=alive'
-				)
-				const data = await response.json()
+    const getPersonajes = async () => {
+      try {
+        const response = await fetch(
+          `https://rickandmortyapi.com/api/character/?name=${filter}`
+        )
+        const data = await response.json()
         console.log(data.results)
-				setPersonajes(data.results)
-				setLoading(false)
-			} catch (error) {
-				console.log(error)
-			}
-		}
-		getPersonajes()
-	}, [])
+        setPersonajes(data.results)
+        setLoading(false)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getPersonajes()
+  }, [filter])
 
   return (
     <div className="container">
       <figure className='logo'>
-				<img src={Logo} alt='Logo de Rick & Morty' />
-			</figure>
-      {/* <header className="App-header">
-        
-      </header> */}
-      
+        <img src={Logo} alt='Logo de Rick & Morty' />
+      </figure>
+
+      <Search filter={filter} setFilter={setFilter} />
+
+      <section className='lista-personajes'>
+        {personajes === undefined || personajes.length === 0 ? (
+          <p>
+            No se encontro personajes con la busqueda{' '}
+            <strong>"{filter}"</strong>.
+          </p>
+        ) : personajes.map((personaje) => (
+          <Card key={personaje.id} personaje={personaje} />
+        ))
+        }
+
+      </section>
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,16 @@
+export const Search = ({ filter, setFilter }) => {
+    const handleInput = ({ target }) => {
+		setFilter(target.value)
+	}
+	return (
+		<section className='filtrar'>
+			<input
+				type='text'
+				placeholder='Nombre del personaje'
+				name='buscar'
+                onChange={handleInput}
+				value={filter}
+			/>
+		</section>
+	)
+}
