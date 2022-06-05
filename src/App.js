fbe07fd541bb2e7c907d997344d377f9688fb657
Natalia2806 <91932527+NataliaTuiran2806@users.noreply.github.com>
2022-06-04 19:46:45 -0500

    LLamado a la API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import Logo from './assets/logo.png'
+import { useState, useEffect } from 'react'
 function App() {
+  const [personajes, setPersonajes] = useState([])
+	const [loading, setLoading] = useState(true)
+	const [filter, setFilter] = useState('')
+
+  //Realizamos llamado a la API
+  useEffect(() => {
+		const getPersonajes = async () => {
+			try {
+				const response = await fetch(
+					'https://rickandmortyapi.com/api/character/?name=Baby&status=alive'
+				)
+				const data = await response.json()
+        console.log(data.results)
+				setPersonajes(data.results)
+				setLoading(false)
+			} catch (error) {
+				console.log(error)
+			}
+		}
+		getPersonajes()
+	}, [])
+
   return (
     <div className="container">
       <figure className='logo'>
